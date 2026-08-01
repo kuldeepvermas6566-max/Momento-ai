a2697acd016abd7ffa7734f967ad73caf10d9d90
Kuldeep kumar verma <kuldeepvermas6566@gmail.com>
2026-08-01 22:39:21 +0530

    Create search.js

---
+++ search.js
@@ -0,0 +1,22 @@
+// ======================================
+// Momento AI
+// Search Engine
+// ======================================
+
+export class SearchEngine{
+
+    search(message){
+
+        return{
+
+            success:false,
+
+            source:"local",
+
+            result:"Search engine is not connected yet."
+
+        };
+
+    }
+
+}
