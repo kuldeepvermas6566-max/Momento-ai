36ff83126e827525aa99a6271553546cda7e788a
Kuldeep kumar verma <kuldeepvermas6566@gmail.com>
2026-08-01 22:34:10 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+// ======================================
+// Momento AI
+// Configuration
+// ======================================
+
+export const CONFIG={
+
+APP_NAME:"Momento AI",
+
+VERSION:"1.0",
+
+MEMORY_LIMIT:100,
+
+AI_NAME:"Momento AI",
+
+DEVELOPER:"Kuldeep Verma",
+
+DEBUG:true
+
+};
